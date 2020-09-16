257ca38bf9ea165f1fde53c267bfc48d702ebbe2
KID <50666207+KID421@users.noreply.github.com>
2020-09-16 20:25:40 +0800

    搜尋最近物件

---
+++ FindNearObject/Assets/FindNearObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class FindNearObject : MonoBehaviour
+{
+    public List<GameObject> balls;
+    public List<float> ballsDistance = new List<float>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Find();
+    }
+
+    private void Find()
+    {
+        balls = GameObject.FindGameObjectsWithTag("球體").ToList();
+
+        for (int i = 0; i < balls.Count; i++) ballsDistance.Add(Vector3.Distance(transform.position, balls[i].transform.position));
+
+        float min = ballsDistance.Min();
+
+        int index = ballsDistance.IndexOf(min);
+
+        balls[index].transform.localScale = Vector3.one * 3;
+    }
+}
---
+++ FindNearObject/Assets/RandomSphere.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class RandomSphere : MonoBehaviour
+{
+    public Transform ball;
+
+    private void Start()
+    {
+        SpawnBall();
+    }
+
+    private void SpawnBall()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            float x = Random.Range(-15f, 15f);
+            float z = Random.Range(-15f, 15f);
+
+            Vector3 pos = new Vector3(x, 5, z);
+
+            Instantiate(ball, pos, Quaternion.identity);
+        }
+    }
+}
